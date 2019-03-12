e914be93607b02d395d150fcac627f598b15e8fe
dmendez <dmendez@genexus.com>
2019-03-12 15:21:30 -0300

    string functions charat/length (multibyte encoding aware)

---
+++ src/text/length.ts
@@ -0,0 +1,3 @@
+export const length = (s: string): number => {
+  return Array.from(s).length;
+};
---
+++ src/text/test/length-cases.ts
@@ -0,0 +1,9 @@
+// length test cases
+type TCases = Array <[string, number]>;
+export const testCases: TCases = [
+  ["texto", 5],
+  ["😀texto",6],
+  [" 😀texto",7],
+  ["Peñarol Campeón del Siglo !!!",29]
+] ;
+
