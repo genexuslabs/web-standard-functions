faacba704a86e48a13119f697f5262c926942a98
dmendez <dmendez@genexus.com>
2019-04-03 15:52:10 -0300

    boolean for Boolean

--- src/text/isMatch.ts
+++ src/text/isMatch.ts
@@ -2,10 +2,10 @@
  * Test a string against regexp
  * @param {string} target
  * @param {string} regExp
- * @return Boolean
+ * @return boolean
  */
 
-export const isMatch = (target: string, regExp: string | RegExp): Boolean => {
+export const isMatch = (target: string, regExp: string | RegExp): boolean => {
   let rex = typeof regExp === "string" ? new RegExp(regExp) : regExp;
   return rex.exec(target) !== null;
 };
