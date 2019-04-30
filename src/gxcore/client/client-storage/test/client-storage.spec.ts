8b18b9c7661046c25ca225dc044f1ab56d934f43
Marcos Crispino <mcrispino@genexus.com>
2019-04-30 14:55:24 -0300

    Added remove method

--- src/gxcore/client/client-storage/test/client-storage.spec.ts
+++ src/gxcore/client/client-storage/test/client-storage.spec.ts
@@ -1,5 +1,6 @@
 import { get } from "../get";
 import { set } from "../set";
+import { remove } from "../remove";
 
 describe("ClientStorage external object", () => {
   it("should store a value and then retrieve it", () => {
@@ -10,6 +11,13 @@ describe("ClientStorage external object", () => {
 
   it("should return the empty string for a non existent key", () => {
     let value = get("nonexistentKey");
+    expect(value).toBe("");
+  });
+
+  it("should remove an existing key", () => {
+    set("myKey", "Hello world!");
+    remove("myKey");
+    let value = get("myKey");
     expect(value).toBe("");
   });
 });
