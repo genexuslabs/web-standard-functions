92d269c9d1cce93c4440cde6f9d08b722f7b4cda
Marcos Crispino <mcrispino@genexus.com>
2019-05-14 18:09:10 -0300

    Added OSName & OSVersion implementations

--- src/gxcore/client/client-information.ts
+++ src/gxcore/client/client-information.ts
@@ -7,6 +7,19 @@ function notImplemented() {
   console.log("Not yet implemented");
 }
 
+function findOSInUserAgent(clientStrings): string {
+  var nAgt = navigator.userAgent;
+  var os = "Unknown";
+  for (var id in clientStrings) {
+    var cs = clientStrings[id];
+    if (cs.r.test(nAgt)) {
+      os = cs.s;
+      break;
+    }
+  }
+  return os;
+}
+
 export class GeneXusClientClientInformation {
   /**
    * This property returns a device identifier
@@ -28,16 +41,90 @@ export class GeneXusClientClientInformation {
    * Returns the operating system name
    */
   static get oSName(): string {
-    notImplemented();
-    return null;
+    return findOSInUserAgent([
+      { s: "Windows", r: /(Windows)/ },
+      { s: "Android", r: /Android/ },
+      { s: "Open BSD", r: /OpenBSD/ },
+      { s: "Sun OS", r: /SunOS/ },
+      { s: "Linux", r: /(Linux|X11)/ },
+      { s: "iOS", r: /(iPhone|iPad|iPod)/ },
+      { s: "Mac OS X", r: /Mac OS X/ },
+      { s: "Mac OS", r: /(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/ },
+      { s: "QNX", r: /QNX/ },
+      { s: "UNIX", r: /UNIX/ },
+      { s: "BeOS", r: /BeOS/ },
+      { s: "OS/2", r: /OS\/2/ },
+      {
+        s: "Search Bot",
+        r: /(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/
+      }
+    ]);
   }
 
   /**
    * Returns the version of the operating system
    */
   static get oSVersion(): string {
-    notImplemented();
-    return null;
+    var os = findOSInUserAgent([
+      { s: "Windows 10", r: /(Windows 10.0|Windows NT 10.0)/ },
+      { s: "Windows 8.1", r: /(Windows 8.1|Windows NT 6.3)/ },
+      { s: "Windows 8", r: /(Windows 8|Windows NT 6.2)/ },
+      { s: "Windows 7", r: /(Windows 7|Windows NT 6.1)/ },
+      { s: "Windows Vista", r: /Windows NT 6.0/ },
+      { s: "Windows Server 2003", r: /Windows NT 5.2/ },
+      { s: "Windows XP", r: /(Windows NT 5.1|Windows XP)/ },
+      { s: "Windows 2000", r: /(Windows NT 5.0|Windows 2000)/ },
+      { s: "Windows ME", r: /(Win 9x 4.90|Windows ME)/ },
+      { s: "Windows 98", r: /(Windows 98|Win98)/ },
+      { s: "Windows 95", r: /(Windows 95|Win95|Windows_95)/ },
+      { s: "Windows NT 4.0", r: /(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/ },
+      { s: "Windows CE", r: /Windows CE/ },
+      { s: "Windows 3.11", r: /Win16/ },
+      { s: "Android", r: /Android/ },
+      { s: "Open BSD", r: /OpenBSD/ },
+      { s: "Sun OS", r: /SunOS/ },
+      { s: "Linux", r: /(Linux|X11)/ },
+      { s: "iOS", r: /(iPhone|iPad|iPod)/ },
+      { s: "Mac OS X", r: /Mac OS X/ },
+      { s: "Mac OS", r: /(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/ },
+      { s: "QNX", r: /QNX/ },
+      { s: "UNIX", r: /UNIX/ },
+      { s: "BeOS", r: /BeOS/ },
+      { s: "OS/2", r: /OS\/2/ },
+      {
+        s: "Search Bot",
+        r: /(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/
+      }
+    ]);
+
+    var osVersion = "Unknown";
+
+    if (/Windows/.test(os)) {
+      osVersion = /Windows (.*)/.exec(os)[1];
+    }
+
+    var nAgt = navigator.userAgent;
+    var nVer = navigator.appVersion;
+    switch (os) {
+      case "Mac OS X":
+        osVersion = /Mac OS X (10[\.\_\d]+)/.exec(nAgt)[1];
+        break;
+
+      case "Android":
+        osVersion = /Android ([\.\_\d]+)/.exec(nAgt)[1];
+        break;
+
+      case "iOS":
+        var osVersionComponents = /OS (\d+)_(\d+)_?(\d+)?/.exec(nVer);
+        osVersion =
+          osVersionComponents[1] +
+          "." +
+          osVersionComponents[2] +
+          "." +
+          (osVersionComponents[3] || "0");
+        break;
+    }
+    return osVersion;
   }
 
   /**
@@ -55,17 +142,14 @@ export class GeneXusClientClientInformation {
     let lang = navigator.languages
       ? navigator.languages[0]
       : navigator.language;
-
     return lang;
   }
 
   /**
-   *
+   * Returns an enumerated value representing the device type
    */
-  private static mdeviceType: any;
-  static get deviceType(): any {
-    notImplemented();
-    return this.mdeviceType;
+  static get deviceType(): number {
+    return 4; // SmartDeviceType.Web
   }
 
   /**
--- src/gxcore/client/test/client-information.spec.ts
+++ src/gxcore/client/test/client-information.spec.ts
@@ -10,16 +10,14 @@ describe("ClientInformation external object", () => {
     let id2 = clientInfo.id;
     expect(id1).toEqual(id2);
   });
-  //   it("should return the OS name", () => {
-  //       let osName = clientInfo.oSName;
-  //       console.log(osName)
-  //       expect(osName.length).not.toBe(0);
-  //   });
-  //   it("should return the OS version", () => {
-  //     let osVersion = clientInfo.oSVersion;
-  //     console.log(osVersion)
-  //     expect(osVersion.length).not.toBe(0);
-  //   });
+  it("should return the OS name", () => {
+    let osName = clientInfo.oSName;
+    expect(osName.length).not.toBe(0);
+  });
+  it("should return the OS version", () => {
+    let osVersion = clientInfo.oSVersion;
+    expect(osVersion.length).not.toBe(0);
+  });
   it("should return the user's language", () => {
     let lang = clientInfo.language;
     expect(lang.length).not.toBe(0);
