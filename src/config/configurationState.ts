09d3b33390a492d48a725d818e3d4a88f2714540
Marcos Crispino <mcrispino@genexus.com>
2019-05-28 15:03:53 -0300

    Added private methods to ConfigurationState to save and read values

--- src/config/configurationState.ts
+++ src/config/configurationState.ts
@@ -1,1 +1,11 @@
-export class ConfigurationState {}
+export class ConfigurationState {
+  // Local storage
+
+  private getStoredValue(key: string): string {
+    return window.localStorage.getItem(key);
+  }
+
+  private setStoredValue(key: string, value: string) {
+    window.localStorage.setItem(key, value);
+  }
+}
