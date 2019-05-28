68567c0fcb29260bad7f05bc8a57c5ca13f85a55
Marcos Crispino <mcrispino@genexus.com>
2019-05-28 15:27:23 -0300

    Added GetLanguage and SetLanguage funcions

--- src/config/configurationState.ts
+++ src/config/configurationState.ts
@@ -1,11 +1,51 @@
+const keyPrefix: string = "gx.config.configurationstate";
+const languageKey: string = "language";
+
 export class ConfigurationState {
+  constructor() {
+    // TODO: default language should be read from the KB settings
+    this.setLanguage(navigator.language);
+  }
+
+  // Language
+
+  /**
+   * Returns the name of the currently active Language object
+   */
+  getLanguage(): string {
+    return this.getStoredValue(languageKey);
+  }
+
+  /**
+   * Sets the language to display literals in your application
+   * @param lang The language to set
+   * @returns 0 if the language can be set, a value greater than 0 if it cannot be set
+   */
+  setLanguage(lang: string): number {
+    if (this.supportedLanguages().includes(lang)) {
+      this.setStoredValue(languageKey, lang);
+      return 0;
+    } else {
+      return 1;
+    }
+  }
+
+  private supportedLanguages(): string[] {
+    // TODO: read from the KB settings
+    return [];
+  }
+
   // Local storage
 
+  private storageKey(key: string): string {
+    return `${keyPrefix}.${key}`;
+  }
+
   private getStoredValue(key: string): string {
-    return window.localStorage.getItem(key);
+    return window.localStorage.getItem(this.storageKey(key));
   }
 
   private setStoredValue(key: string, value: string) {
-    window.localStorage.setItem(key, value);
+    window.localStorage.setItem(this.storageKey(key), value);
   }
 }
