2a4df12436aa8d28e537740b987b9c370264cc86
dmendez@genexus <dmendez@genexus.com>
2019-05-29 15:47:03 -0300

    Datetime functions

--- src/datetime/test/time.spec.ts
+++ src/datetime/test/time.spec.ts
@@ -1,4 +1,4 @@
-import { time, time_format } from "../time";
+import { timeImpl, time_format } from "../time";
 
 export const testCases: Array<[time_format, Date, String]> = [
   [time_format.format_12, new Date(1891, 8, 28, 13, 10, 10, 10), "1:10:10 PM"],
@@ -12,7 +12,7 @@ describe("time operation", () => {
     it(`time for ${t[1]} with format ${t[0]} should be equal to "${
       t[2]
     }"`, () => {
-      expect(time(t[0], t[1])).toEqual(t[2]);
+      expect(timeImpl(t[0], t[1])).toEqual(t[2]);
     });
   }
 });
