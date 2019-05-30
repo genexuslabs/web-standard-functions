4a6b9ae377b703d120241396a877b83810d19baa
dmendez@genexus <dmendez@genexus.com>
2019-05-30 11:30:20 -0300

    setempty implementation and fix at milliseconds test

--- src/datetime/test/millisecond.spec.ts
+++ src/datetime/test/millisecond.spec.ts
@@ -1,18 +1,18 @@
-import { second } from "../second";
+import { millisecond } from "../millisecond";
 
 export const testCases: Array<[Date, number]> = [
-  [new Date(1891, 8, 28, 1, 0, 0), 0],
-  [new Date(1891, 8, 28, 1, 0, 1), 1],
-  [new Date(1891, 8, 28, 1, 0, 15), 15],
-  [new Date(1891, 8, 28, 1, 0, 20), 20],
-  [new Date(1891, 8, 28, 1, 0, 60), 0],
-  [new Date(1891, 8, 28, 1, 0, 61), 1]
+  [new Date(1891, 8, 28, 1, 0, 0, 0), 0],
+  [new Date(1891, 8, 28, 1, 0, 0, 1), 1],
+  [new Date(1891, 8, 28, 1, 0, 0, 15), 15],
+  [new Date(1891, 8, 28, 1, 0, 0, 20), 20],
+  [new Date(1891, 8, 28, 1, 0, 0, 1000), 0],
+  [new Date(1891, 8, 28, 1, 0, 0, 1001), 1]
 ];
 
-describe("second operation", () => {
+describe("millisecond operation", () => {
   for (const t of testCases) {
-    it(`second for "${t[0]}" should be equal to "${t[1]}"`, () => {
-      expect(second(t[0])).toEqual(t[1]);
+    it(`millisecond for "${t[0]}" should be equal to "${t[1]}"`, () => {
+      expect(millisecond(t[0])).toEqual(t[1]);
     });
   }
 });
