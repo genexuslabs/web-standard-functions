3e5add87dee4770bb57bab7d8ce9301b2b5398db
dmendez@genexus <dmendez@genexus.com>
2019-06-04 11:36:38 -0300

    Better text for now with a implementation fix

--- src/datetime/now.ts
+++ src/datetime/now.ts
@@ -3,18 +3,12 @@
  * @return Date
  */
 
-import { DateTime } from "luxon";
 import { fromTimezone } from "./fromTimezone";
-import { toUniversalTime } from "./toUniversalTime";
 import { timezones } from "./timezone";
 import { minutesToMilliseconds } from "./core";
 
 export const now = (): Date => {
   let date = new Date();
   let offset = minutesToMilliseconds(date.getTimezoneOffset());
-
-  return fromTimezone(
-    toUniversalTime(new Date(date.getTime() + offset)),
-    timezones.UTC
-  );
+  return fromTimezone(new Date(date.getTime() + offset), timezones.UTC);
 };
