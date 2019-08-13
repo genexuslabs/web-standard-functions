65ed9beca34df54d7b67a670101dff10b43bfee4
Marcos Crispino <mcrispino@genexus.com>
2019-08-13 17:03:56 -0300

    Refactored ChooseImage function

--- src/gxcore/sd/media/photoLibrary/chooseImage.ts
+++ src/gxcore/sd/media/photoLibrary/chooseImage.ts
@@ -1,20 +1,9 @@
-import { ResolverFunc, publishCall } from "../../../../misc/publishCall";
+import { pickMultimedia } from "../common/pickMultimedia";
 
 /**
  * Chooses an image from the photo library
  * @return {string} image
  */
 export const chooseImage = async (): Promise<string> => {
-  let resolver = (
-    opt: string,
-    image: string,
-    resolve: ResolverFunc<string>
-  ) => {
-    if (opt === "ok") {
-      resolve(image);
-    } else {
-      resolve(null);
-    }
-  };
-  return publishCall<string>("chooseImage", ["ok", "cancel"], resolver);
+  return pickMultimedia("chooseImage");
 };
