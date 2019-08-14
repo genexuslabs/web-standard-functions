26fc7b0a0989458aa4c9a3568b0ff2fe57eb2e42
Marcos Crispino <mcrispino@genexus.com>
2019-08-14 11:53:53 -0300

    Added Calendar.Schedule method definition

---
+++ src/gxcore/sd/calendar/schedule.ts
@@ -0,0 +1,21 @@
+import { notImplemented } from "../../../misc/helpers";
+
+/**
+ * Allows scheduling some task on the end user's calendar. Every parameter is optional except for `title` and `startDate`.
+ * @param {string} title
+ * @param startDate
+ * @param endDate
+ * @param startTime
+ * @param endTime
+ * @param place
+ */
+export function schedule(
+  title: any,
+  startDate: Date,
+  endDate: Date,
+  startTime: any,
+  endTime: any,
+  place: any
+) {
+  notImplemented();
+}
