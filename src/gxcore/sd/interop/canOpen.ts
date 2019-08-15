43869aa3f3e753fd264bb23930e122a67199505b
Marcos Crispino <mcrispino@genexus.com>
2019-08-15 10:12:15 -0300

    Improvements to Interop.CanOpen

--- src/gxcore/sd/interop/canOpen.ts
+++ src/gxcore/sd/interop/canOpen.ts
@@ -1,13 +1,20 @@
+import { ConfigurationState } from "../../../config/configurationState";
+
+const knownProtocols = ["http:", "https:", "mailto:", "tel:"];
+
 /**
  * Checks if an URL can be opened
- * Current implementation only returns `true` for http and https URLs
- * @param url
+ * @param {string} urlStr
  */
-export const canOpen = (url: string): boolean => {
-  return (
-    url.startsWith("http://") ||
-    url.startsWith("https://") ||
-    url.startsWith("tel:") ||
-    url.startsWith("mailto:")
+export const canOpen = (urlStr: string): boolean => {
+  let baseURL = ConfigurationState.getInstance().getDynStoredValue(
+    "SERVICE_HOSTNAME"
   );
+  try {
+    let url = baseURL ? new URL(urlStr, baseURL) : new URL(urlStr);
+    return knownProtocols.includes(url.protocol);
+  } catch (e) {
+    // could not construct URL object
+    return false;
+  }
 };
