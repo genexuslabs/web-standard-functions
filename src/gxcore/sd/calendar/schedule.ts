cf4e3cac7a6fca43fdf78ff6b04aa37d8bbdfaa6
Marcos Crispino <mcrispino@genexus.com>
2019-08-16 14:59:28 -0300

    Made suggested changes from code review

--- src/gxcore/sd/calendar/schedule.ts
+++ src/gxcore/sd/calendar/schedule.ts
@@ -1,4 +1,3 @@
-import { notImplemented } from "../../../misc/helpers";
 import { addHours } from "../../../datetime/addHours";
 import { GUID } from "../../../types/guid";
 
@@ -19,10 +18,10 @@ export const schedule = (
   endTime: Date,
   place: string
 ) => {
-  let sDateTime = getDateTime(startDate, startTime);
-  let eDateTime = getDateTime(endDate, endTime, addHours(sDateTime, 1));
+  const sDateTime = getDateTime(startDate, startTime);
+  const eDateTime = getDateTime(endDate, endTime, addHours(sDateTime, 1));
 
-  let calStr = createCalendarEvent(sDateTime, eDateTime, title, place);
+  const calStr = createCalendarEvent(sDateTime, eDateTime, title, place);
 
   downloadCalendar(calStr);
 };
@@ -42,7 +41,7 @@ function convertDateTimeToString(dt: Date): string {
 }
 
 function stringFromTimeNumber(num: number): string {
-  return (num < 10 ? "0" : "") + num.toString();
+  return num.toString().padStart(2, "0");
 }
 
 function getDateTime(
@@ -54,7 +53,7 @@ function getDateTime(
     return defaultValue;
   }
 
-  let result = datePart;
+  const result = datePart;
   if (timePart) {
     result.setHours(timePart.getHours());
     result.setMinutes(timePart.getMinutes());
@@ -63,55 +62,34 @@ function getDateTime(
   return result;
 }
 
-export function createCalendarEvent(
+export const createCalendarEvent = (
   start: Date,
   end: Date,
   title: string,
   place: string
-): string {
-  return (
-    "BEGIN:VCALENDAR" +
-    "\n" +
-    "VERSION:2.0" +
-    "\n" +
-    // + "PRODID:-//sebbo.net//ical-generator//EN" + "\n"
-    "BEGIN:VEVENT" +
-    "\n" +
-    "UID:" +
-    getGUIDString() +
-    "\n" +
-    "SEQUENCE:0" +
-    "\n" +
-    "DTSTAMP:" +
-    convertDateTimeToString(new Date()) +
-    "\n" +
-    "DTSTART:" +
-    convertDateTimeToString(start) +
-    "\n" +
-    "DTEND:" +
-    convertDateTimeToString(end) +
-    "\n" +
-    "SUMMARY:" +
-    title +
-    "\n" +
-    "LOCATION:" +
-    place +
-    "\n" +
-    "END:VEVENT" +
-    "\n" +
-    "END:VCALENDAR"
-  );
-}
+) =>
+  `BEGIN:VCALENDAR
+VERSION:2.0
+BEGIN:VEVENT
+UID:${getGUIDString()}
+SEQUENCE:0
+DTSTAMP:${convertDateTimeToString(new Date())}
+DTSTART:${convertDateTimeToString(start)}
+DTEND:${convertDateTimeToString(end)}
+SUMMARY:${title}
+LOCATION:${place}
+END:VEVENT
+END:VCALENDAR`;
 
 function getGUIDString(): string {
   return GUID.newGuid().toString();
 }
 
 function downloadCalendar(calendarStr: string) {
-  let guidStr = getGUIDString();
-  let fileName = guidStr + ".ics";
+  const guidStr = getGUIDString();
+  const fileName = guidStr + ".ics";
 
-  var element = document.createElement("a");
+  const element = document.createElement("a");
   element.setAttribute(
     "href",
     "data:text/plain;charset=utf-8," + encodeURIComponent(calendarStr)
