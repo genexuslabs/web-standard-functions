bae20d534cfbf14e2628c474430922d670bd0245
Marcos Crispino <mcrispino@genexus.com>
2019-12-13 10:53:25 -0300

    Defined constants for browserId regular expresions
    Also: removed duplicated code

--- src/web/browserCommon.ts
+++ src/web/browserCommon.ts
@@ -7,39 +7,11 @@ export function browserIdFromAgent(userAgent: string): BrowserResult {
     return { id: BrowserIdValues.UnknownAgent, version: "" };
   }
 
-  let matches = userAgent.match(/msie\/([0-9\.]+)/i);
-  if (matches !== null) {
-    return { id: BrowserIdValues.InternetExplorer, version: matches[1] };
-  }
-
-  matches = userAgent.match(/edge\/([0-9\.]+)/i);
-  if (matches !== null) {
-    return { id: BrowserIdValues.Edge, version: matches[1] };
-  }
-
-  matches = userAgent.match(/chrome\/([0-9\.]+)/i);
-  if (matches !== null) {
-    return { id: BrowserIdValues.Chrome, version: matches[1] };
-  }
-
-  matches = userAgent.match(/safari\/([0-9\.]+)/i);
-  if (matches !== null) {
-    return { id: BrowserIdValues.Safari, version: matches[1] };
-  }
-
-  matches = userAgent.match(/opera\/([0-9\.]+)/i);
-  if (matches !== null) {
-    return { id: BrowserIdValues.Opera, version: matches[1] };
-  }
-
-  matches = userAgent.match(/firefox\/([0-9\.]+)/i);
-  if (matches !== null) {
-    return { id: BrowserIdValues.MozillaFirefox, version: matches[1] };
-  }
-
-  matches = userAgent.match(/netscape\/([0-9\.]+)/i);
-  if (matches !== null) {
-    return { id: BrowserIdValues.Netscape, version: matches[1] };
+  for (const brMatcher of browserMatchers) {
+    let matches = userAgent.match(brMatcher.regExp);
+    if (matches !== null) {
+      return { id: brMatcher.browserId, version: matches[1] };
+    }
   }
 
   return { id: BrowserIdValues.UnknownAgent, version: "" };
@@ -60,3 +32,26 @@ export enum BrowserIdValues {
   Safari = 8,
   Edge = 9
 }
+
+interface BrowserMatcher {
+  regExp: RegExp;
+  browserId: BrowserIdValues;
+}
+
+const msieRegEx = /msie\/([0-9\.]+)/i;
+const edgeRegEx = /edge\/([0-9\.]+)/i;
+const chromeRegEx = /chrome\/([0-9\.]+)/i;
+const safariRegEx = /safari\/([0-9\.]+)/i;
+const operaRegEx = /opera\/([0-9\.]+)/i;
+const firefoxRegEx = /firefox\/([0-9\.]+)/i;
+const netscapeRegEx = /netscape\/([0-9\.]+)/i;
+
+const browserMatchers: Array<BrowserMatcher> = [
+  { regExp: msieRegEx, browserId: BrowserIdValues.InternetExplorer },
+  { regExp: edgeRegEx, browserId: BrowserIdValues.Edge },
+  { regExp: chromeRegEx, browserId: BrowserIdValues.Chrome },
+  { regExp: safariRegEx, browserId: BrowserIdValues.Safari },
+  { regExp: operaRegEx, browserId: BrowserIdValues.Opera },
+  { regExp: firefoxRegEx, browserId: BrowserIdValues.MozillaFirefox },
+  { regExp: netscapeRegEx, browserId: BrowserIdValues.Netscape }
+];
