d683374aad652a7a8ea28c6737725ccb4f69e7ef
ag-silva <33836926+ag-silva@users.noreply.github.com>
2024-02-21 15:59:26 -0300

    Removed deprecated serialization functions

--- src/types/type-serialization.ts
+++ src/types/type-serialization.ts
@@ -8,34 +8,3 @@ export interface ISerializable {
 export const isSerializable = (obj: any) => {
   return typeof obj == "object" && "serialize" in obj && "deserialize" in obj;
 };
-
-// Decorators to control serialization of SDTs
-export const serialization = (sender: string) => {
-  return function(target: any, propertyKey: string) {
-    if (!target._gxSerializationInfo) {
-      target._gxSerializationInfo = {};
-    }
-    target._gxSerializationInfo[propertyKey] = sender;
-  };
-};
-
-// Functions to obtain serialization info
-export const findPropertyInstanceName = (inst, prop) => {
-  if (inst["_gxSerializationInfo"]) {
-    const serializatinInfo = inst["_gxSerializationInfo"];
-    for (const p in serializatinInfo) {
-      if (serializatinInfo[p] === prop) {
-        return p;
-      }
-    }
-  }
-  return prop;
-};
-
-export const findPropertyObjectName = (inst, prop) => {
-  if (inst["_gxSerializationInfo"]) {
-    const serializatinInfo = inst["_gxSerializationInfo"];
-    return serializatinInfo[prop] ?? prop;
-  }
-  return prop;
-};
