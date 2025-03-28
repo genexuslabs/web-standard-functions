1fb99f9ba3b335f189093d3c50aadc3c307b03aa
Maria Laura Gonzalez Noguez <mlgonzalez@genexus.com>
2025-03-28 10:04:06 -0300

    issue203923 - type-conversions date,datetime fixed

--- src/types/gxdatetime.ts
+++ src/types/gxdatetime.ts
@@ -1,18 +1,46 @@
 import { ISerializable } from "./type-serialization";
 import { ConfigurationState } from "../config/configurationState";
-import { SerializeDatetimeToISOString } from "./type-conversions/serializeDatetimeToISOString";
-import { DeserializeISOStringToDatetime } from "./type-conversions/deserializeISOStringToDatetime";
+import { datetimeToISOString } from "./type-conversions/datetimeToISOString";
+import { isEmpty } from "./type-conversions/isEmpty";
+import { localToUTC } from "./type-conversions/localToUTC";
+import { DTAToDate } from "./type-conversions/dTAToDate";
+import { ISODateToDTA } from "./type-conversions/iSODateToDTA";
+import { isValidDate } from "./type-conversions/isValidDate";
 
 export class GxDatetime extends Date implements ISerializable {
   serialize() {
     let utc = ConfigurationState.getInstance().getConvertTimeFromUTC();
-    return SerializeDatetimeToISOString(this, utc);
+    return GxDatetime.serializeUTC(this, utc);
   }
 
-  deserialize(isoDate: string) {
+  deserialize(isoDatetime: string) {
     let utc = ConfigurationState.getInstance().getConvertTimeFromUTC();
-    const dt = DeserializeISOStringToDatetime(isoDate, utc);
-    return new GxDatetime(dt);
+    return GxDatetime.deserializeUTC(isoDatetime, utc);
+  }
+
+  static serializeUTC(datetime: Date, utcToLocal = false) {
+    if (!isEmpty(datetime)) {
+      if (utcToLocal) {
+        return datetimeToISOString(localToUTC(datetime));
+      } else {
+        return datetimeToISOString(datetime);
+      }
+    }
+    return "0000-00-00T00:00:00";
+  }
+
+  static deserializeUTC(isoDatetime: string, utcToLocal = false) {
+    let d: Date;
+    try {
+      const da = ISODateToDTA(isoDatetime);
+      d = DTAToDate(da, utcToLocal);
+    } catch {
+      throw new Error('Invalid date: "' + isoDatetime + '"');
+    }
+    if (!isValidDate(d)) {
+      d = new Date(0, 0, 0);
+    }
+    return new GxDatetime(d);
   }
 
   static createFromDate(d: Date): GxDatetime {
@@ -20,8 +48,7 @@ export class GxDatetime extends Date implements ISerializable {
   }
 
   fromISOString(isoDate: string): GxDatetime {
-    const dt = DeserializeISOStringToDatetime(isoDate, false);
-    return new GxDatetime(dt);
+    return GxDatetime.deserializeUTC(isoDate, false);
   }
 
   static compare(GxDatetime1: GxDatetime, GxDatetime2: GxDatetime): boolean {
--- src/types/type-conversions/deserializeISOStringToDatetime.ts
+++
@@ -1,8 +0,0 @@
-import { datetimeFromISOString } from "./datetimeFromISOString";
-
-export const DeserializeISOStringToDatetime = (
-  s: string,
-  convertTimeFromUTC
-): Date => {
-  return datetimeFromISOString(s, convertTimeFromUTC);
-};
