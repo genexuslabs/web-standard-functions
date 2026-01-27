5a3220f6cfb763b622d302e57cb6db9059bef8c2
Maria Laura Gonzalez Noguez <m.gonzaleznoguez@globant.com>
2026-01-27 14:22:47 -0300

    confirm test fixed

--- src/misc/test/confirm.spec.ts
+++ src/misc/test/confirm.spec.ts
@@ -6,7 +6,7 @@ describe("confirm test", () => {
   it("should return true when confirmed", async () => {
     let confSubscription = subscribe(`${prefix}_confirm`, (...data: any[]) => {
       expect(data.length).toBe(2);
-      expect(data[1]).toEqual("Confirm?");
+      expect(data[1]).toEqual(["Confirm?", "ok", "cancel"]);
       cancelSubscription(confSubscription);
 
       let guid = data[0];
@@ -19,7 +19,7 @@ describe("confirm test", () => {
   it("should return false when canceled", async () => {
     let confSubscription = subscribe(`${prefix}_confirm`, (...data: any[]) => {
       expect(data.length).toBe(2);
-      expect(data[1]).toEqual("Confirm?");
+      expect(data[1]).toEqual(["Confirm?", "ok", "cancel"]);
       cancelSubscription(confSubscription);
 
       let guid = data[0];
@@ -27,6 +27,34 @@ describe("confirm test", () => {
     });
 
     let result = await confirm("Confirm?");
+    expect(result).toBe(false);
+  });
+
+  it("should return true when confirmed, yes", async () => {
+    let confSubscription = subscribe(`${prefix}_confirm`, (...data: any[]) => {
+      expect(data.length).toBe(2);
+      expect(data[1]).toEqual(["Confirm?", "yes", "no"]);
+      cancelSubscription(confSubscription);
+
+      let guid = data[0];
+      publish(`${prefix}_confirm_${guid}_yes`);
+    });
+
+    let result = await confirm("Confirm?", "yes", "no");
+    expect(result).toBe(true);
+  });
+
+  it("should return false when canceled, no", async () => {
+    let confSubscription = subscribe(`${prefix}_confirm`, (...data: any[]) => {
+      expect(data.length).toBe(2);
+      expect(data[1]).toEqual(["Confirm?", "yes", "no"]);
+      cancelSubscription(confSubscription);
+
+      let guid = data[0];
+      publish(`${prefix}_confirm_${guid}_no`);
+    });
+
+    let result = await confirm("Confirm?", "yes", "no");
     expect(result).toBe(false);
   });
 });
